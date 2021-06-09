803427b18bbad8e2822194c46dd072d167fb9ec9
Mary Saray <msaray.ms@outlook.com>
2021-06-09 11:41:25 -0700

    Implemented AJAX.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 window.onload = function () {
     var btnJoke = document.getElementById("get-joke");
-    btnJoke.onclick = getRandomJoke;
+    btnJoke.onclick = main;
 };
-function getRandomJoke() {
+function main() {
+    var http = new XMLHttpRequest();
+    http.open("GET", "https://api.icndb.com/jokes/random?limitTo=[nerdy]");
+    http.onreadystatechange = proccesRequest;
+    http.send();
+}
+function proccesRequest() {
+    var http = this;
+    if (http.readyState == 4 && http.status == 200) {
+        document.getElementById("display-joke").innerHTML =
+            http.responseText;
+    }
 }
--- ts/main.ts
+++ ts/main.ts
@@ -2,7 +2,7 @@
 window.onload = function(){
     // set up button click
     let btnJoke = document.getElementById("get-joke");
-    btnJoke.onclick = getRandomJoke;
+    btnJoke.onclick = main;
 }
 /**
  * This function is associated with the onclick event 
@@ -14,7 +14,35 @@ window.onload = function(){
  * Each time the button is clicked a new joke
  * will be generated and displayed.
  */
-function getRandomJoke(){
+function main(){
+    // Create object to communicate with the server.
+    let http = new XMLHttpRequest();
+    // Specifies the type of request: get URL from Chuck Norris API.
+    http.open("GET", "https://api.icndb.com/jokes/random?limitTo=[nerdy]");
+
+    // Function to retrieve differets readyState status.
+    http.onreadystatechange = proccesRequest;
+
+    // Send request to the server
+    http.send();
+   
     // get joke from API
     // display joke
+}
+/**
+ * checks the status and returns a specific result 
+ * related to the status message### that has been return.
+ * Results depends on the status message### and
+ * what is being applied within the control structure.
+ */
+function proccesRequest(){
+    let http = <XMLHttpRequest>this;
+    /* 4: request finished and response is ready. */
+    /* 200 OK: The request is OK --> standard response 
+               for successful HTTP requests) */
+    if(http.readyState == 4 && http.status == 200)// both needs to be true.
+    {
+        document.getElementById("display-joke").innerHTML =
+                                http.responseText;
+    }
 }
