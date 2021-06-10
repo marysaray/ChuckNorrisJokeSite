e3a83f8bf4c66d51532bb6a2bf76a116e5cfab28
Mary Saray <msaray.ms@outlook.com>
2021-06-09 19:02:13 -0700

    Added functions to retrieve categories and display

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
+var oneJoke = (function () {
+    function oneJoke() {
+    }
+    return oneJoke;
+}());
 window.onload = function () {
     var btnJoke = document.getElementById("get-joke");
     btnJoke.onclick = main;
+    getAllCategories();
 };
 function main() {
     var http = new XMLHttpRequest();
@@ -11,7 +17,47 @@ function main() {
 function proccesRequest() {
     var http = this;
     if (http.readyState == 4 && http.status == 200) {
-        document.getElementById("display-joke").innerHTML =
-            http.responseText;
+        var response = JSON.parse(http.responseText).value;
+        console.log(response);
+        console.log(response.id);
+        console.log(response.joke);
+        console.log(response.categories);
+        displayJoke(response);
+    }
+}
+function displayJoke(j) {
+    var displayDiv = document.getElementById("display-joke");
+    var jokeIdSpan = displayDiv.querySelector("h2 > span");
+    jokeIdSpan.innerText = j.id.toString();
+    var jokeParagraph = displayDiv.querySelector("h3");
+    jokeParagraph.innerHTML = j.joke;
+    var liCategory = displayDiv.querySelector("ul");
+    liCategory.innerHTML = "";
+    for (var i = 0; i < j.categories.length; i++) {
+        var nxtCategory = document.createElement("h4");
+        nxtCategory.innerText = "[ " + j.categories[i] + " ]";
+        liCategory.appendChild(nxtCategory);
+    }
+}
+function getAllCategories() {
+    var http = new XMLHttpRequest();
+    http.open("GET", "https://api.icndb.com/categories");
+    http.onreadystatechange = displayCategories;
+    http.send();
+}
+function displayCategories() {
+    var http = this;
+    if (http.readyState == 4 && http.status == 200) {
+        var categories = JSON.parse(http.responseText).value;
+        console.log(http.responseText);
+        console.log(categories);
+        var boxCateogry = "";
+        for (var i = 0; i < categories.length; i++) {
+            var cat = categories[i];
+            boxCateogry += "<input type=\"checkbox\" value=\"" + cat + "\" id=\"" + cat + "\">\n            <label for=\"" + cat + "\">" + cat + "</label>";
+        }
+        console.log(boxCateogry);
+        var categoriesDiv = document.getElementById("categories");
+        categoriesDiv.innerHTML = boxCateogry;
     }
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,8 +1,26 @@
+// Create custom class to give intellisens.
+class oneJoke{
+    /**
+     * The number associated with the joke.
+     */
+    id:number;
+    /**
+     * The text of the joke.
+     */
+    joke:string;
+    /**
+     * The category title in the array list.
+     */
+    categories:string[];
+}
+
 // when the web page is done loading 
 window.onload = function(){
     // set up button click
     let btnJoke = document.getElementById("get-joke");
     btnJoke.onclick = main;
+
+    getAllCategories();
 }
 /**
  * This function is associated with the onclick event 
@@ -25,9 +43,6 @@ function main(){
 
     // Send request to the server
     http.send();
-   
-    // get joke from API
-    // display joke
 }
 /**
  * checks the status and returns a specific result 
@@ -40,9 +55,77 @@ function proccesRequest(){
     /* 4: request finished and response is ready. */
     /* 200 OK: The request is OK --> standard response 
                for successful HTTP requests) */
-    if(http.readyState == 4 && http.status == 200)// both needs to be true.
-    {
-        document.getElementById("display-joke").innerHTML =
-                                http.responseText;
+    if(http.readyState == 4 && http.status == 200){// both needs to be true.
+    // convert string into object with JSON parse method.
+    let response:oneJoke = JSON.parse(http.responseText).value; // get value out of response
+        console.log(response);    
+        console.log(response.id);
+        console.log(response.joke);
+        console.log(response.categories);
+        displayJoke(response); 
     }
 }
+/**
+ * This function will display the joke on the web page.
+ * @param J takes in object 
+ */
+function displayJoke(j:oneJoke){
+    // get div to manipulate
+    let displayDiv = document.getElementById("display-joke");
+    // select the element you want to target
+    let jokeIdSpan = <HTMLElement>displayDiv.querySelector("h2 > span");
+    // display value of object id.
+    jokeIdSpan.innerText = j.id.toString();
+    // select the element you want to target
+    let jokeParagraph = displayDiv.querySelector("h3");
+    // display value of object joke.
+    jokeParagraph.innerHTML = j.joke;
+    // select element you want to target
+    let liCategory = displayDiv.querySelector("ul");
+    // grab list of the web page
+    // clear out the list
+    liCategory.innerHTML = "";
+    // remake category list
+    // go through a loop to get each category
+    for(let i = 0; i < j.categories.length; i++) {
+        // create new element in memory
+        let nxtCategory = document.createElement("h4");
+        // select category in the list
+        nxtCategory.innerText = "[ " + j.categories[i] + " ]"; 
+        // add category to the element on the web page.
+        liCategory.appendChild(nxtCategory);
+    }
+}
+// Retrieve all categories 
+function getAllCategories(){
+    // object to communicate with the server
+    let http = new XMLHttpRequest();
+    http.open("GET", "https://api.icndb.com/categories");
+    http.onreadystatechange = displayCategories;
+    http.send();
+}
+/**
+ * This function will execute everytime a new category 
+ * has been selected from the web page.
+ */
+function displayCategories(){
+    let http = <XMLHttpRequest>this;
+    if(http.readyState == 4 && http.status == 200){
+        let categories:string[] = JSON.parse(http.responseText).value;
+        console.log(http.responseText);
+        console.log(categories);
+        let boxCateogry = "";
+        for(let i = 0; i < categories.length; i++){
+            let cat = categories[i];
+            // this is an example of how programmers wrote code before template literals.
+            // boxCateogry += "<input type='checkbox' value='" + cat + " 'id='" +
+            // cat + "'> <label for='"> + cat + "'>" +cat + "<\label>";
+            boxCateogry += `<input type="checkbox" value="${cat}" id="${cat}">
+            <label for="${cat}">${cat}</label>`
+        }
+        console.log(boxCateogry);
+        let categoriesDiv = document.getElementById("categories");
+        categoriesDiv.innerHTML = boxCateogry;
+    }
+}
+
